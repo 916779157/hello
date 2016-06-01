d2c32471702473de9b5fd47514b8460cd10f2f58
“““916779157” <916779157@qq.com>
2016-06-02 02:37:14 +0800

    first commit

--- Best Time to Buy and Sell Stock II.js
+++ Best Time to Buy and Sell Stock II.js
@@ -1,17 +1,16 @@
-Best Time to Buy and Sell Stock Ⅱ
+/*Best Time to Buy and Sell Stock Ⅱ
 最佳时间买入和卖出股票让、Ⅱ
+*/
 
 /**
  * @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function(prices) 
-{
- var profit = 0;
-  for(var i = 1; i < prices.length; i++)
-  {
-  if(prices[i] >prices[i-1])    
-  profit += prices[i]-prices[i-1];
+ {var profit = 0;
+  for(var i = 1; i < prices.length; i++){
+       if(prices[i] >prices[i-1]){
+          profit += prices[i]-prices[i-1];}
   }
   return profit;
 };
---
+++ Reverse String.js
@@ -0,0 +1,21 @@
+/*网上的直接算法*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) { 
+  return s.split('').reverse().join('');
+};
+
+
+/*我自己写的便于理解*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) { 
+  var stack = [];
+  for(var len = s.length,i=len;i>=0;i-- ){
+      stack.push(s[i]);}
+      return stack.join('');
+};
