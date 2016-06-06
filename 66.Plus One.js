676b7514522c5b2ee401cc59f8d5bf0c42a7d163
“““916779157” <916779157@qq.com>
2016-06-07 00:26:36 +0800

    first commit

--- 66.Plus One.js
+++ 66.Plus One.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var plusOne = function(digits){
-  for (var i = digits.length - 1; i >= 0; -- i) {
+  for (var i = digits.length - 1; i >= 0; --i) {
     if (digits[i] <= 8) {
       digits[i] = digits[i] + 1;
-        return digits;
+    return digits;
     } else {
       if (i !== 0) {
         digits[i] = 0;
