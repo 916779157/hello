335d53b39412446d17f1e85fe36aa6cf3f1cb962
“““916779157” <916779157@qq.com>
2016-06-12 00:47:03 +0800

    first commit

---
+++ 283. Move Zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  var pos =0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[pos] = nums[i];
+      pos++;   
+    }
+  }
+ for (pos;pos < nums.length; pos++) {
+  nums[pos] = 0;    
+ }
+};
